c437e0309eac5e27b19fff6eee13559d3216acd5
Denis Salopek <denis.sale@gmail.com>
2015-04-09 12:20:14 +0200

    Fix install_click.sh script for FreeBSD 10.1.

--- scripts/install_click.sh
+++ scripts/install_click.sh
@@ -1,8 +1,11 @@
+#!/bin/sh
+
 # Command that is used to install click modular router software into the
 # virtual node template. The template is fetched form:
 # http://imunes.net/dl/click.tar.gz
 
 click="click.tar.gz"
+workdir="/tmp/vroot_prepare/tools"
 
 if [ "$1" == "" ]; then
     echo "The command shoud be called with the destination vroot folder:"
@@ -10,12 +13,16 @@ if [ "$1" == "" ]; then
     exit
 fi
 
-cd /tmp
+LDFLAGS=""
+if [ "`uname -r | cut -d'-' -f1`" = "10.1" ]; then
+    export LDFLAGS="-L/usr/lib -lexecinfo"
+fi
+
+mkdir -p $workdir
+cd $workdir
 if [ ! -f $click ]; then 
     fetch http://imunes.net/dl/$click
 fi
 tar xf $click
 cd click
-./configure --prefix=$1/usr/local --enable-etherswitch
-make install-userlevel
-cd /tmp && rm -fr click
+./configure --prefix=$1/usr/local --enable-etherswitch && make install-userlevel
