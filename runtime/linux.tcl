c8074d46f4e2faa66b4310d0bd1cdd3ab06798cd
Goran Cetusic <goran.cetusic@gmail.com>
2015-05-24 17:59:59 +0200

    added connecting of switches and vimages

--- runtime/linux.tcl
+++ runtime/linux.tcl
@@ -118,7 +118,13 @@ proc createLinkBetween { lnode1 lnode2 ifname1 ifname2 link } {
     upvar 0 ::cf::[set ::curcfg]::eid eid
 
     # FIXME: max bridgename length is 15
-    exec ovs-vsctl add-br $eid.$link
+    if { [[typemodel $lnode1].virtlayer] == "OPENVSWITCH" && \
+        [[typemodel $lnode2].virtlayer] == "OPENVSWITCH"} {
+        exec ovs-vsctl add-port $eid.$lnode1 $eid.$lnode1.$ifname1 -- set interface $eid.$lnode1.$ifname1 type=patch options:peer=$eid.$lnode2.$ifname2
+        exec ovs-vsctl add-port $eid.$lnode2 $eid.$lnode2.$ifname2 -- set interface $eid.$lnode2.$ifname2 type=patch options:peer=$eid.$lnode1.$ifname1
+    } else {
+        exec ovs-vsctl add-br $eid.$link
+    }
 }
 
 proc configureLinkBetween { lnode1 lnode2 ifname1 ifname2 link } {}
@@ -132,9 +138,14 @@ proc startIfcsNode { node } {
         if { $ifc != "lo0" } {
             set mtu [getIfcMTU $node $ifc]
             if {[getIfcOperState $node $ifc] == "up"} {
-                set peer_node [logicalPeerByIfc $node $ifc]
-                set link [linkByPeers $node $peer_node]
-                exec pipework $eid.$link -i $ifc $node_id 0/0
+                set peerNode [logicalPeerByIfc $node $ifc]
+                if { [[typemodel $peerNode].layer] == "LINK" } {
+                    exec pipework $eid.$peerNode -i $ifc $node_id 0/0
+                }
+                if { [[typemodel $peerNode].layer] == "NETWORK" } {
+                    set link [linkByPeers $node $peerNode]
+                    exec pipework $eid.$link -i $ifc $node_id 0/0
+                }
             }
         }
     }
