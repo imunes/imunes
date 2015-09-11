8db07035fd0a40d168c835239de320dc8f1e7911
denisSal <denis.sale@gmail.com>
2015-09-11 12:35:05 +0000

    upgraded versioning scheme
    When installing IMUNES from the git repo, we display the lastet tag as IMUNES version.
    Added 'v' in front of the version number.

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -8,6 +8,8 @@ ver_file="$ROOTDIR/$LIBDIR/VERSION"
 git status > /dev/null 2>&1
 
 if [ "$?" -eq 0 ]; then
+    tag=`git describe --abbrev=0 --tags`
+    sed -i"" "s/VERSION: .*/VERSION: $tag/" $ver_file
     for attr in "%h" "%ai" "%ae"; do
 	value=`git log --format="$attr" -n 1`
 	sed -i"" -e "s/\$Format:$attr\$$/$value/" $ver_file
