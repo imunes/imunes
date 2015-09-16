02e0104dda2bc2bb9939316aada6559fa1f31b76
oikuda <valter.vasic@gmail.com>
2015-09-16 12:38:51 +0200

    added space to please FreeBSD sed command

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -9,9 +9,9 @@ git status > /dev/null 2>&1
 
 if [ "$?" -eq 0 ]; then
     tag=`git describe --abbrev=0 --tags`
-    sed -i"" "s/VERSION: .*/VERSION: $tag/" $ver_file
+    sed -i "" "s/VERSION: .*/VERSION: $tag/" $ver_file
     for attr in "%h" "%ai" "%ae"; do
 	value=`git log --format="$attr" -n 1`
-	sed -i"" -e "s/\$Format:$attr\$$/$value/" $ver_file
+	sed -i "" -e "s/\$Format:$attr\$$/$value/" $ver_file
     done
 fi
