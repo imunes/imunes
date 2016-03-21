4b446281d67b4c6ada21de15ee10b6d85c4b080b
oikuda <valter.vasic@gmail.com>
2016-03-21 11:25:41 +0100

    changed running exp detection
    Reverted back to old behaviour using himage.

--- runtime/linux.tcl
+++ runtime/linux.tcl
@@ -235,12 +235,9 @@ proc spawnShell { node cmd } {
 #****
 proc fetchRunningExperiments {} {
     catch {exec himage -l | cut -d " " -f 1} exp_list
-    catch {exec ovs-vsctl list-br | cut -d- -f1 | uniq} exp_list2
     set exp_list [split $exp_list "
 "]
-    set exp_list2 [split $exp_list2 "
-"]
-    return "$exp_list $exp_list2"
+    return "$exp_list"
 }
 
 #****f* linux.tcl/allSnapshotsAvailable
