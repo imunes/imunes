imunes/imunes

commit b5e407688ab373a5b451acda3086dcf1c753053e

added a check for missing node types
When loading a topology, exit if there are some nodes which are not supported
on the platform running the experiment.

runtime/cfgparse.tcl
@@ -285,6 +285,7 @@ proc loadCfg { cfg } {
     global showBkgImage showGrid showAnnotations
     global iconSize
     global hostsAutoAssign
+    global execMode all_modules_list
 
     # Cleanup first
     set node_list {}
@@ -720,6 +721,18 @@ proc loadCfg { cfg } {
     set IPv4UsedList ""
     set MACUsedList ""
     foreach node $node_list {
+	set nodeType [typemodel $node]
+	if { $nodeType ni $all_modules_list && ! [string match "router.*" $nodeType] } {
+	    set msg "Unknown node type: '$nodeType'."
+	    if { $execMode == "batch" } {
+		statline $msg
+	    } else {
+		tk_dialog .dialog1 "IMUNES warning" \
+		    "Error: $msg" \
+		info 0 Dismiss
+	    }
+	    exit
+	}
 	if { "lo0" ni [logIfcList $node] && \
 		[[typemodel $node].layer] == "NETWORK"} {
 	    setLogIfcType $node lo0 lo

commit ee64b154295cf41674ea1a24e9c93b529e3a7111

fix bug introduced by b5e407688ab373a5b
The check didn't include the pseudo node type, and this prevented topologies
with pseudo nodes to load into IMUNES.

runtime/cfgparse.tcl
@@ -722,7 +722,8 @@ proc loadCfg { cfg } {
     set MACUsedList ""
     foreach node $node_list {
 	set nodeType [typemodel $node]
-	if { $nodeType ni $all_modules_list && ! [string match "router.*" $nodeType] } {
+	if { $nodeType ni [concat $all_modules_list "pseudo"] && \
+	    ! [string match "router.*" $nodeType] } {
 	    set msg "Unknown node type: '$nodeType'."
 	    if { $execMode == "batch" } {
 		statline $msg