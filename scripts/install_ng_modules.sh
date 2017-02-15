a6b5a56ec810dd2091ce089f723dc0086221f4e7
denisSal <denis.sale@gmail.com>
2017-02-15 13:32:23 +0100

    modified ng_modules install script
    The default kernel module install directory in FreeBSD is changed from
    /boot/kernel to /boot/modules, so KMODDIR is set to /boot/kernel when
    installing the modules.

--- scripts/install_ng_modules.sh
+++ scripts/install_ng_modules.sh
@@ -6,5 +6,5 @@ export MFLAGS=""
 export MAKEFLAGS=""
 
 for module in rfee patmat source; do
-    cd src/ng_$module && make && make install && cd -
+    cd src/ng_$module && make && make KMODDIR=/boot/kernel install && cd -
 done
