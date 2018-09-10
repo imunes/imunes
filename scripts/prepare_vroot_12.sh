867dd3e5e2d86714cc63f8db39ec47ceb8ad9960
denisSal <denis.sale@gmail.com>
2018-09-10 15:50:44 +0200

    first version of IMUNES for FreeBSD 12
    FreeBSD 12 is in ALPHA-5, so this is still in testing phase...
    Removed zfs support from scripts, applied patch from issue #72.
    Also dropping support for older FreeBSD versions from now on.
    
    Some examples fail for now, but most functionality works.

---
+++ scripts/prepare_vroot_12.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+. scripts/prepare_vroot_functions.sh
+
+PACKAGES_MINIMAL="$PACKAGES_MINIMAL bind913"
+PACKAGES="$PACKAGES_MINIMAL $PACKAGES_COMMON isc-dhcp44-server isc-dhcp44-client \
+    sylpheed xorp firefox wireshark gnome-themes-standard"
+
+checkArgs $*
+
+# Start installation
+mkdir -p $WORKDIR
+cd $WORKDIR
+echo -n "" > $LOG
+
+if [ $mini -eq 1 ]; then
+    PKGS=${PACKAGES_MINIMAL}
+else
+    PKGS=${PACKAGES}
+fi
+
+if [ $offline -eq 0 ]; then
+    fetchBaseOnline
+fi
+
+prepareUnionfs
+populateFs
+
+preparePackagesPkg
+chroot $VROOT_MASTER /bin/sh -c 'env ASSUME_ALWAYS_YES=YES pkg bootstrap' >> $LOG 2>&1
+checkPkgVersion
+installPackagesPkg
+
+if [ $mini -eq 0 ]; then
+    log "OUT" "Installing additional tools..."
+    sh $IMUNESDIR/scripts/install_usr_tools.sh >> $LOG 2>&1
+    log "OUT" "Installing additional tools done."
+fi
+
+configQuagga
+configXorp
+
+wiresharkGUIfix
+
+cleanUnnecessary
+
+log "OUT" "Installation successfully finished. Check the log for more \
+information: $LOG"
