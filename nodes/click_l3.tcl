a0ad09cd8bce8d097108fda3886dd795760ae391
José Manuel Romero Herrera <81595794+jromero17@users.noreply.github.com>
2021-04-02 17:56:37 -0400

    Se agregó soporte de traducción a otros idiomas

--- nodes/click_l3.tcl
+++ nodes/click_l3.tcl
@@ -459,8 +459,8 @@ proc $MODULE.configGUI { c node } {
     set guielements {}
 
     configGUI_createConfigPopupWin $c
-    wm title $wi "click_l3 configuration"
-    configGUI_nodeName $wi $node "Node name:"
+    wm title $wi [mc "click_l3 configuration"]
+    configGUI_nodeName $wi $node [mc "Node name:"]
 
     set tabs [configGUI_addNotebook $wi $node {"Configuration" "Interfaces"}]
     set configtab [lindex $tabs 0]
