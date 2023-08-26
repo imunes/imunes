fd199df230e42dc17f4194371316dbb2f19963eb
José Manuel Romero Herrera <81595794+jromero17@users.noreply.github.com>
2023-08-26 11:42:44 -0400

    Update eventedit.tcl Added support for .svg extension icons. .gif icons are no longer supported.
    Added support for .svg extension icons.
    .gif icons are no longer supported.

--- legacy/eventedit.tcl
+++ legacy/eventedit.tcl
@@ -29,7 +29,8 @@ foreach b {select const ramp square rand} {
     global eventedit_img
 
     set eventedit_img($b) \
-	[image create photo -file $ROOTDIR/$LIBDIR/icons/tiny/$b.gif]
+	[image create photo -file $ROOTDIR/$LIBDIR/icons/tiny/$b.svg]
+ 	#[image create photo -file $ROOTDIR/$LIBDIR/icons/tiny/$b.gif]		
 }
 
 #****f* eventedit.tcl/eventedit
