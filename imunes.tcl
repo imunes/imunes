d0eaecf40911844d608817d03a58ab9f0bb221a9
DaouiWalid <124696120+DaouiWalid@users.noreply.github.com>
2024-04-21 21:24:11 +0100

    Update imunes.tcl

--- imunes.tcl
+++ imunes.tcl
@@ -157,14 +157,14 @@ foreach file [glob -directory $ROOTDIR/$LIBDIR/runtime *.tcl] {
 # Set default L2 node list
 set l2nodes "hub lanswitch click_l2 rj45 stpswitch filter packgen ext"
 # Set default L3 node list
-set l3nodes "genericrouter quagga xorp static click_l3 host pc nat64 nouveauPc nouveauRouteur AP STA"
+set l3nodes "genericrouter quagga xorp static click_l3 host pc Pclone nat64 nouveauPc nouveauRouteur AP STA"
 # Set default supported router models
 set supp_router_models "xorp quagga static"
 
 if { $isOSlinux } {
     # Limit default nodes on linux
     set l2nodes "lanswitch rj45 ext"
-    set l3nodes "genericrouter quagga static host pc nat64 nouveauPc nouveauRouteur AP STA"
+    set l3nodes "genericrouter quagga static host pc Pclone nat64 nouveauPc nouveauRouteur AP STA"
     set supp_router_models "quagga static"
     safeSourceFile $ROOTDIR/$LIBDIR/runtime/linux.tcl
     if { $initMode == 1 } {
