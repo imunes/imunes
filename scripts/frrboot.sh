cadcf281f68bc29e4a789dad6082499728fe2da4
José Manuel Romero Herrera <81595794+jromero17@users.noreply.github.com>
2024-09-29 07:41:47 -0400

    Create frrboot.sh

---
+++ scripts/frrboot.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+zebra -dP0
+
+for f in rip ripng ospf ospf6; do
+    grep -q "router $f\$" $1 && ${f}d -dP0
+done
+
+for f in bgp isis; do
+    grep -q "router $f .*\$" $1 && ${f}d -dP0
+done
+
+sed -i '' '/Disabling MPLS support/d' /terr.log
+
+vtysh << __END__
+conf term
+`cat $1`
+__END__
