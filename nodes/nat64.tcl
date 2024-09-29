e7b8b754f2ae6b9511750245f789542584fd2012
José Manuel Romero Herrera <81595794+jromero17@users.noreply.github.com>
2024-09-29 07:15:46 -0400

    Fix nat64 node

--- nodes/nat64.tcl
+++ nodes/nat64.tcl
@@ -127,8 +127,9 @@ proc $MODULE.virtlayer {} {
 }
 
 proc $MODULE.cfggen { node } {
-    set cfg [router.quagga.cfggen $node]
-
+    #set cfg [router.quagga.cfggen $node]
+    set cfg [router.frr.cfggen $node]
+    
     upvar 0 ::cf::[set ::curcfg]::eid eid
     global nat64ifc_$eid.$node
     if { [info exists nat64ifc_$eid.$node] == 0 } {
@@ -153,15 +154,29 @@ proc $MODULE.cfggen { node } {
 }
 
 proc $MODULE.bootcmd { node } {
-    return [router.quagga.bootcmd $node] 
+    #return [router.quagga.bootcmd $node] 
+    return [router.frr.bootcmd $node]
 }
 
 proc $MODULE.shellcmds { } {
-    return [router.quagga.shellcmds]
+    #return [router.quagga.shellcmds]
+    return [router.frr.shellcmds]
 }
 
 proc $MODULE.instantiate { eid node } {
-    router.quagga.instantiate $eid $node
+    #router.quagga.instantiate $eid $node
+    router.frr.instantiate $eid $node
+}
+
+proc $MODULE.setupNamespace { eid node } {
+    l3node.setupNamespace $eid $node
+}
+proc $MODULE.initConfigure { eid node } {
+    l3node.initConfigure $eid $node
+    enableIPforwarding $eid $node
+}
+proc $MODULE.createIfcs { eid node ifcs } {
+    l3node.createIfcs $eid $node $ifcs
 }
 
 proc $MODULE.start { eid node } {
@@ -170,7 +185,8 @@ proc $MODULE.start { eid node } {
     set tun [createStartTunIfc $eid $node]
     set nat64ifc_$eid.$node $tun
 
-    router.quagga.start $eid $node
+    #router.quagga.start $eid $node
+    router.frr.start $eid $node
 
     set datadir "/var/db/tayga"
 
@@ -192,11 +208,13 @@ proc $MODULE.start { eid node } {
 
     # XXX
     # Even though this routes should be added here, we add them in the
+    ## router.quagga.start procedure which invokes nat64.cfggen where we define
     # router.quagga.start procedure which invokes nat64.cfggen where we define
     # them with:
     # lappend cfg "ip route $tayga4pool $tun"
     # lappend cfg "ipv6 route $tayga6prefix $tun"
-    # This is done in order for quagga to redistribute these routes.
+    ## This is done in order for quagga to redistribute these routes.
+    # This is done in order for frr to redistribute these routes.
     # FreeBSD:
     # exec jexec $eid.$node route -n add -inet $tayga4pool -interface $tun
     # exec jexec $eid.$node route -n add -inet6 $tayga6prefix -interface $tun
@@ -208,17 +226,20 @@ proc $MODULE.start { eid node } {
 }
 
 proc $MODULE.shutdown { eid node } {
-    router.quagga.shutdown $eid $node
+    #router.quagga.shutdown $eid $node
+    router.frr.shutdown $eid $node
     taygaShutdown $eid $node
 }
 
 proc $MODULE.destroy { eid node } {
     taygaDestroy $eid $node
-    router.quagga.destroy $eid $node
+    #router.quagga.destroy $eid $node
+    router.frr.destroy $eid $node
 }
 
 proc $MODULE.nghook { eid node ifc } {
-    return [router.quagga.nghook $eid $node $ifc] 
+    #return [router.quagga.nghook $eid $node $ifc] 
+    return [router.frr.nghook $eid $node $ifc]
 }
 
 
@@ -236,7 +257,7 @@ proc $MODULE.configGUI { c node } {
     set ifctab [lindex $tabs 1]
     set nat64tab [lindex $tabs 2]
 
-    set treecolumns {"OperState State" "IPv4addr IPv4 addr" "IPv6addr IPv6 addr" \
+    set treecolumns {"OperState State" "NatState Nat" "IPv4addr IPv4 addr" "IPv6addr IPv6 addr" \
             "MACaddr MAC addr" "MTU MTU" "QLen Queue len" "QDisc Queue disc" "QDrop Queue drop" }
     configGUI_addTree $ifctab $node
 
