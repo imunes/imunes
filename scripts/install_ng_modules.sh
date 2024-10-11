e6f26ed2b3a933954a86606ba01fbc230878b235
José Manuel Romero Herrera <81595794+jromero17@users.noreply.github.com>
2024-10-11 10:19:59 -0400

    Update install_ng_modules.sh

--- scripts/install_ng_modules.sh
+++ scripts/install_ng_modules.sh
@@ -9,6 +9,11 @@ if test -z "$KMODDIR"; then
     export KMODDIR="/boot/kernel"
 fi
 
+if test -z "$(ls /usr/src/ 2>/dev/null)"; then
+	echo 'Kernel source not installed in /usr/src/. Skipping ng_* module building.'
+	exit 0
+fi
+
 for module in rfee patmat source; do
     cd src/ng_$module && make && make install && cd -
 done
