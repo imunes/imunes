84fd98c164922ece8161b6a9452089708af09127
José Manuel Romero Herrera <81595794+jromero17@users.noreply.github.com>
2024-10-11 10:21:24 -0400

    Update install_usr_tools.sh

--- scripts/install_usr_tools.sh
+++ scripts/install_usr_tools.sh
@@ -8,7 +8,6 @@ export MAKEFLAGS=""
 VROOT_MASTER=/var/imunes/vroot
 IMUNESDIR=`pwd`
 
-click="http://imunes.net/dl/click.tar.gz"
 tayga="http://www.litech.org/tayga/tayga-0.9.2.tar.bz2"
 workdir="/tmp/vroot_prepare/tools"
 
@@ -29,21 +28,3 @@ patch < $IMUNESDIR/src/patches/tayga_fbsd_patch.diff
 
 cp tayga $VROOT_MASTER/usr/local/bin 
 echo "Installing tayga done." 
-
-# Build and install click
-echo "Installing click..." 
-cd $workdir
-VERSION=`uname -r | cut -d'-' -f1 | cut -d'.' -f1`
-
-if [ ! -f `basename $click` ]; then 
-    fetch $click
-fi
-
-if [ $VERSION -ge 10 ]; then
-    export LDFLAGS="-L/usr/lib -lexecinfo"
-fi
-
-tar xf `basename $click`
-cd click
-./configure --prefix=$VROOT_MASTER/usr/local --enable-etherswitch && make install-userlevel
-echo "Installing click done." 
