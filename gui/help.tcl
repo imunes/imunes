3125aa3fa17421fcf2ca82d5c116a5b0df5d68ae
José Manuel Romero Herrera <81595794+jromero17@users.noreply.github.com>
2024-10-11 05:58:20 -0400

    Update help.tcl

--- gui/help.tcl
+++ gui/help.tcl
@@ -35,7 +35,6 @@
 #  This file is considered to contain all the help information.
 #  Currently it contains only copyright information.
 #****
-
 set copyright {
 
 Copyright 2004-2014 University of Zagreb.
