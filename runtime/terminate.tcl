0feaf28657a0d7beec545560191cf55a065ee840
Denis Salopek <denisSal@users.noreply.github.com>
2024-12-01 13:23:59 +0100

    Introduce trigger_* procedures
    Used to trigger nodes/links/interfaces creation, destruction,
    recreation, or configuration, unconfiguration or reconfiguration.
    
    When custom config is enabled or when auto default routes is disabled on
    node, do not automatically reconfigure them. This would usually happen
    when connecting/disconnecting a router/extnat node in the same subnet or
    when changing some router/extnats interface IP address.
    
    Also, do not reconfigure if custom config is selected and the user
    toggles auto default routes in node config.

--- runtime/terminate.tcl
+++ runtime/terminate.tcl
@@ -136,14 +136,17 @@ proc linksDestroy { eid links links_count w } {
 	    set lnode2 [lindex [getLinkPeers $mirror_link] 0]
 	}
 
-	try {
-	    if { [getLinkDirect $link_id] } {
-		destroyDirectLinkBetween $eid $lnode1 $lnode2
-	    } else {
-		destroyLinkBetween $eid $lnode1 $lnode2 $link_id
+	if { [getFromRunning "${link_id}_running"] == true } {
+	    try {
+		if { [getLinkDirect $link_id] } {
+		    destroyDirectLinkBetween $eid $lnode1 $lnode2
+		} else {
+		    destroyLinkBetween $eid $lnode1 $lnode2 $link_id
+		}
+		setToRunning "${link_id}_running" false
+	    } on error err {
+		return -code error "Error in 'destroyLinkBetween $eid $lnode1 $lnode2 $link_id': $err"
 	    }
-	} on error err {
-	    return -code error "Error in 'destroyLinkBetween $eid $lnode1 $lnode2 $link_id': $err"
 	}
 
 	incr batchStep
@@ -172,11 +175,18 @@ proc terminate_nodesDestroy { eid nodes nodes_count w } {
     foreach node_id $nodes {
 	displayBatchProgress $batchStep $nodes_count
 
-	try {
-	    [getNodeType $node_id].nodeDestroy $eid $node_id
-	    setToRunning "${node_id}_running" false
-	} on error err {
-	    return -code error "Error in '[getNodeType $node_id].nodeDestroy $eid $node_id': $err"
+	if { [getNodeType $node_id] != "pseudo" && [getFromRunning "${node_id}_running"] in "true delete" } {
+	    try {
+		[getNodeType $node_id].nodeDestroy $eid $node_id
+	    } on error err {
+		return -code error "Error in '[getNodeType $node_id].nodeDestroy $eid $node_id': $err"
+	    }
+
+	    if { [getFromRunning "${node_id}_running"] == "delete" } {
+		unsetRunning "${node_id}_running"
+	    } else {
+		setToRunning "${node_id}_running" false
+	    }
 	}
 	pipesExec ""
 
@@ -254,6 +264,8 @@ proc undeployCfg { { eid "" } { terminate 0 } } {
 
 	set $var ""
     }
+
+    prepareTerminateVars
 
     if { "$terminate_nodes$destroy_nodes_ifaces$terminate_links$unconfigure_links$unconfigure_nodes_ifaces$unconfigure_nodes" == "" } {
 	setToExecuteVars "terminate_cfg" ""
@@ -571,15 +583,7 @@ proc terminate_nodesIfacesUnconfigure { eid nodes_ifaces nodes_count w } {
 	}
 	displayBatchProgress $batchStep $nodes_count
 
-	if { [getAutoDefaultRoutesStatus $node] == "enabled" } {
-	    lassign [getDefaultGateways $node $subnet_gws $nodes_l2data] my_gws subnet_gws nodes_l2data
-	    lassign [getDefaultRoutesConfig $node $my_gws] all_routes4 all_routes6
-
-	    setDefaultIPv4routes $node $all_routes4
-	    setDefaultIPv6routes $node $all_routes6
-	}
-
-	if { [info procs [getNodeType $node].nodeIfacesUnconfigure] != "" } {
+	if { [info procs [getNodeType $node].nodeIfacesUnconfigure] != "" && [getFromRunning "${node}_running"] in "true delete" } {
 	    try {
 		[getNodeType $node].nodeIfacesUnconfigure $eid $node $ifaces
 	    } on error err {
@@ -618,10 +622,12 @@ proc terminate_nodesIfacesDestroy { eid nodes_ifaces nodes_count w } {
 		set ifaces [ifcList $node]
 	    }
 
-	    try {
-		[getNodeType $node].nodeIfacesDestroy $eid $node $ifaces
-	    } on error err {
-		return -code error "Error in '[getNodeType $node].nodeIfacesDestroy $eid $node $ifaces': $err"
+	    if { [getFromRunning "${node}_running"] in "true delete" } {
+		try {
+		    [getNodeType $node].nodeIfacesDestroy $eid $node $ifaces
+		} on error err {
+		    return -code error "Error in '[getNodeType $node].nodeIfacesDestroy $eid $node $ifaces': $err"
+		}
 	    }
 	}
 
