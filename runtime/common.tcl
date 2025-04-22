80ba5d75d72ddfbd7dee70b8df41a4e113b101d2
Denis Salopek <denisSal@users.noreply.github.com>
2025-04-22 17:15:36 +0200

    Skip changing directory on Attach
    TODO: cwd will be used in some future feature

--- runtime/common.tcl
+++ runtime/common.tcl
@@ -941,7 +941,7 @@ proc resumeSelectedExperiment { exp } {
     setToRunning "current_file" [getExperimentConfigurationFromFile $exp]
     openFile
     readRunningVarsFile $exp
-    catch { cd [getFromRunning "cwd"] }
+    #catch { cd [getFromRunning "cwd"] }
 
     setToRunning "eid" $exp
     setToRunning "cfg_deployed" true
