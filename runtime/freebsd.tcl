ea2bc52cda366c650a82af7774284b3d84adcf9a
Karlo Miličević <karlo98.m@gmail.com>
2025-05-09 12:35:40 +0200

    Merge extnat node into ext
    NAT interface selection is added to extnat configuration.
    If UNASSIGNED no NAT rules are added.
    Display label is changed to "NAT-$nat_interface" when not UNASSIGNED.

--- runtime/freebsd.tcl
+++ runtime/freebsd.tcl
@@ -1279,7 +1279,7 @@ proc isNodeStarted { node_id } {
 
     set node_type [getNodeType $node_id]
     if { [$node_type.virtlayer] != "VIRTUALIZED" } {
-	if { $node_type in "rj45 ext extnat" } {
+	if { $node_type in "rj45 ext" } {
 	    return true
 	}
 
@@ -1337,7 +1337,7 @@ proc nodePhysIfacesCreate { node_id ifaces } {
 	set iface_name [getIfcName $node_id $iface_id]
 	set public_hook $node_id-$iface_name
 	set prefix [string trimright $iface_name "0123456789"]
-	if { $node_type in "ext extnat" } {
+	if { $node_type in "ext" } {
 	    set iface_name $node_id
 	}
 
