d7e7f83245de40bd03b755a13d8dd188c0843525
Denis Salopek <denisSal@users.noreply.github.com>
2025-07-16 20:25:22 +0200

    Fix condition block for ext in removeIface

--- config/ifaces.tcl
+++ config/ifaces.tcl
@@ -1187,9 +1187,9 @@ proc removeIface { node_id iface_id } {
 					removeFilterIfcRule $node_id $other_iface_id $rule_num
 				}
 			}
-		} elseif { $node_type in "ext" && [getNodeNATIface $node_id] != "UNASSIGNED" } {
-			trigger_nodeUnconfig $node_id
 		}
+	} elseif { $node_type in "ext" && [getNodeNATIface $node_id] != "UNASSIGNED" } {
+		trigger_nodeUnconfig $node_id
 	} elseif { $node_type in "lanswitch" && [getNodeVlanFiltering $node_id] } {
 		foreach other_iface_id [ifcList $node_id] {
 			if { $iface_id != $other_iface_id && [getIfcVlanType $node_id $other_iface_id] == "trunk" } {
