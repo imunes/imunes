4649661daa8b5dcdabaad3bd51348c480ef33438
Denis Salopek <denisSal@users.noreply.github.com>
2026-03-11 17:44:06 +0100

    Fix convert_n2st.sh script

--- icons/convert_n2st.sh
+++ icons/convert_n2st.sh
@@ -11,12 +11,12 @@ small_opt="-adaptive-resize $sfact"
 tiny_opt="-adaptive-resize $tfact -gravity center -background none -extent $tsize"
 
 for img in $images; do
-    if [ ! -e small/$img ] || [ normal/$img -nt small/$img ]; then
-	magick $small_opt normal/$img small/$img
-	echo "Converting normal/$img to small."
-    fi
-    if [ ! -e tiny/$img ] || [ normal/$img -nt tiny/$img ]; then
-	magick $tiny_opt normal/$img tiny/$img
-	echo "Converting normal/$img to tiny."
-    fi
+	if [ ! -e small/$img ] || [ normal/$img -nt small/$img ]; then
+		magick normal/$img $small_opt small/$img
+		echo "Converting normal/$img to small."
+	fi
+	if [ ! -e tiny/$img ] || [ normal/$img -nt tiny/$img ]; then
+		magick normal/$img $tiny_opt tiny/$img
+		echo "Converting normal/$img to tiny."
+	fi
 done
